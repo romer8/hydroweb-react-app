7aee483075ec8287cdd7d64cbe5d8562ccb6941a
romer8 <gio.busrom@gmail.com>
2022-08-26 14:23:29 -0600

    change in design for the legend

--- src/SideMenuWrapper/SideMenuWrapper.js
+++ src/SideMenuWrapper/SideMenuWrapper.js
@@ -1,22 +1,16 @@
 import React from "react";
 import {SideMenu} from '../styles/SideMenu.styled'
-import Dropdown from "react-bootstrap/Dropdown";
-import DropdownButton from "react-bootstrap/DropdownButton";
-import "bootstrap/dist/css/bootstrap.min.css";
 
-const SideMenuWrapper = ({ style, onLayer,layer,opacity_wms,wms_op_val}) => {
+const SideMenuWrapper = ({ onLayer,layer}) => {
 
-  const opacityHandler = (data) => {
 
-    opacity_wms(data);
-  }
 
   return(
     
       <SideMenu>
         <div className="wrapper_absolute">
         <p className="sudo_title">
-          Landslides Events
+          Legend
         </p>
         <div className="mycontainer">
           
@@ -27,26 +21,7 @@ const SideMenuWrapper = ({ style, onLayer,layer,opacity_wms,wms_op_val}) => {
           onChange={(event) => onLayer(event)}
           className="input_name"
         /> 
-        <span>Hide/show</span>
-        <div className="mainContainer">
-        <p className="sudo_title">
-          Landslides Zones
-        </p>
-        <div className="mycontainer">
-          <p className="prompt">Contrast</p>
-          <div className="buble"> 
-              {wms_op_val}
-          </div>
-          <div className="slider-parent">
-              <input type="range" min="0" max="1"  step="0.1" value={wms_op_val}
-                onChange={({ target: { value: radius } }) => {
-                            opacityHandler(radius);
-                          }}
-              />
-          </div>
-        </div> 
-
-        </div>
+        <span>Virtual Stations</span>
 
         </div>        
 
