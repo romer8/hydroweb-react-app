8273fd8dc5c7b721dfb54f61d65a02520347e584
romer8 <gio.busrom@gmail.com>
2022-09-29 11:06:40 -0600

    added one more button to Extrapolate the GeoGloWs Forecast

--- src/Extra/Button_group.js
+++ src/Extra/Button_group.js
@@ -11,10 +11,12 @@ const Button_group = ({ executeGeoglows, executeBiasCorrection,executeHydroWebDa
     
       <ButtonGroup isFullMap={isFullMap}>
         <div className="multi-button">
-            <button onClick={executeHydroWebData} id="hydroweb_data"> <span>1</span> Hydroweb Data</button>
-            <button onClick={executeGeoglows} id="historical_simulation"><span>2</span> Historical Simulation Data</button>
-            <button onClick={executeBiasCorrection} id="bias_correction"><span>3</span> Execute Bias Correction</button>
-            <button id="data_download"><span>4</span> Download Data</button>
+            <button onClick={executeHydroWebData} id="hydroweb_data"> <span>1</span> Obtain Hydroweb Data</button>
+            <button onClick={executeGeoglows} id="historical_simulation"><span>2</span> GEOGloWS Historical Simulation Data</button>
+            <button onClick={executeBiasCorrection} id="bias_correction"><span>3</span> Execute GEOGloWS Bias Correction</button>
+            <button onClick={executeBiasCorrection} id="forecast_extrapolation"><span>4</span> Extrapolate GEOGloWS Forecast </button>
+
+            <button id="data_download"><span>5</span> Download Data</button>
         </div>
       </ButtonGroup>
 
