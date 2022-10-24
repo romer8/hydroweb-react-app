ff390f4edf2ef345d3c9ec78bfb41a36e5fa4c67
romer8 <gio.busrom@gmail.com>
2022-10-24 17:29:03 -0600

    not showing the download data

--- src/Extra/Button_group.js
+++ src/Extra/Button_group.js
@@ -17,7 +17,7 @@ const Button_group = ({ executeGeoglows, executeBiasCorrection,executeHydroWebDa
             <button onClick={executeGeoglows} id="historical_simulation"><span className="numberCircle">2</span> GEOGloWS Historical Simulation Data<FontAwesomeIcon icon={faCircleCheck} /></button>
             <button onClick={executeBiasCorrection} id="bias_correction"><span className="numberCircle">3</span> Execute GEOGloWS Bias Correction <FontAwesomeIcon icon={faCircleCheck} /></button>
             <button onClick={executeForecast} id="forecast_extrapolation"><span className="numberCircle">4</span> Extrapolate GEOGloWS Forecast <FontAwesomeIcon icon={faCircleCheck} /></button>
-            <button id="data_download"><span>5</span> Download Data</button>
+            {/* <button id="data_download"><span>5</span> Download Data</button> */}
         </div>
       </ButtonGroup>
 
